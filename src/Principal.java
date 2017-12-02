ef60b82a1b7af70f24298ab3e13ca671f19148f6
admin <admin@OSMARVM0>
2017-12-02 12:53:16 -0200

    Correção de Comentários

--- src/Principal.java
+++ src/Principal.java
@@ -2,7 +2,7 @@
  * Universidade Federal de Santa Catarina - UFSC
  * Departamento de Informática e Estatística - INE
  * Programa de Pós-Graduação em Ciências da Computação - PROPG
- * Disciplinas: Projeto e Análise de Algoritmos
+ * Disciplina: Projeto e Análise de Algoritmos
  * Prof Alexandre Gonçalves da Silva 
  * Baseado nos slides 70 da aula do dia 22/09/2017 
  * Algoritmo de Karatsuba: versão final, n arbitrário
